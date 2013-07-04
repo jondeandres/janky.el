a34f1d55aaafc223dc37ed26e74743fbd2fc06d1
Jon de Andres <jondeandres@gmail.com>
2013-07-05 00:55:29 +0200

    Add `janky-request` bsaic function to make requests to the janky server and parse the response.

--- janky.el
+++ janky.el
@@ -1,13 +1,47 @@
 (require 'url)
 (require 'json)
 
-(defvar janky-url "")
+(eval-when-compile (require 'cl))
 
-(defun janky-request (url)
+(defcustom janky-auth-user ""
+  "The user for auth basic authorization"
+  :group 'janky
+  :type 'string)
+
+(defcustom janky-auth-password ""
+  "The password for auth basic authorization"
+  :group 'janky
+  :type 'string)
+
+(defcustom janky-base-url ""
+  "The janky url"
+  :group 'janky
+  :type 'string)
+
+(defun janky-extra-headers ()
+  `(("Authorization" . , (concat "Basic "
+                                 (base64-encode-string
+                                  (concat janky-auth-user
+                                          ":"
+                                          janky-auth-password))))))
+
+(defun* janky-request (url callback &key (format :json) (method "GET"))
   "Makes a request to URL and returns a parsed json object"
-  (with-current-buffer (url-retrieve-synchronously janky-url)
-    (goto-char (point-min))
-    (re-search-forward "^$")
-    (let ((json-object-type 'plist))
-      (json-read))))
+  (let ((url-request-method method)
+        (json-object-type 'plist)
+        (url-request-extra-headers (janky-extra-headers))
+        (data)
+        (raw-data)
+        (status))
+    (with-current-buffer (url-retrieve-synchronously url)
+      (goto-char (point-min))
+      (re-search-forward "^$")
+      (setq status url-http-response-status)
+      (setq raw-data (buffer-substring (point) (point-max)))
+      (setq data (if (equal format :json)
+                     (json-read-from-string raw-data)
+                   raw-data))
+      (if callback
+          (funcall callback status data)
+        data))))
 
