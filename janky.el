c7296b35092724b0149ecc0613d082565a57fe40
Jon de Andres <jondeandres@gmail.com>
2014-03-06 01:58:22 +0100

    Refactor for most of the functions.
    Split them into smaller functions..

--- janky.el
+++ janky.el
@@ -18,32 +18,31 @@
   :group 'janky
   :type 'string)
 
-(defun janky-extra-headers ()
-  `(("Authorization" . , (concat "Basic "
-                                 (base64-encode-string
-                                  (concat janky-auth-user
-                                          ":"
-                                          janky-auth-password))))))
+(defun janky-base64-auth ()
+  (base64-encode-string
+   (concat janky-auth-user ":" janky-auth-password)))
 
-(defun* janky-request (url callback &key (format :json) (method "GET"))
+(defun janky-extra-headers ()
+  `(("Authorization" . ,(concat "Basic " (janky-base64-auth)))))
+
+(defun* janky-request (url &key (format :json) (method "GET"))
   "Makes a request to URL and returns a parsed json object"
   (let ((url-request-method method)
         (json-object-type 'plist)
-        (url-request-extra-headers (janky-extra-headers))
-        (data)
-        (raw-data)
-        (status))
+        (url-request-extra-headers (janky-extra-headers)))
     (with-current-buffer (url-retrieve-synchronously url)
       (goto-char (point-min))
       (re-search-forward "^$")
-      (setq status url-http-response-status)
-      (setq raw-data (buffer-substring (point) (point-max)))
-      (setq data (if (equal format :json)
-                     (json-read-from-string raw-data)
-                   raw-data))
-      (if callback
-          (funcall callback status data)
-        data))))
+      (janky-parse-response (buffer-substring (point) (point-max))
+                            format))))
+
+(defun janky-parse-response (raw-data format)
+  (if (equal format :json)
+      (json-read-from-string raw-data)
+    raw-data))
+
+(defun janky-replace-json-false (data)
+  (mapcar (lambda (item) (if (equal :json-false item) nil item)) data))
 
 (defun janky-repo-branch-path (repo branch)
   (concat janky-base-url repo "/" branch))
@@ -53,15 +52,25 @@
         (branch (read-string "Branch: ")))
     (list repo branch)))
 
-(defun janky-ci-build (repo branch)
+(defun janky-build (repo branch)
   (interactive (janky-read-repo-branch-args))
   (janky-request (janky-repo-branch-path repo branch)
-                 nil
-                 :format :raw :method "POST"))
+                 :format :raw
+                 :method "POST"))
 
-(defun janky-ci-status (repo branch)
+(defun janky-status (repo branch)
   (interactive (janky-read-repo-branch-args))
-  (janky-request (janky-repo-branch-path repo branch)
-                        nil))
+  (janky-request (janky-repo-branch-path repo branch)))
+
+(defun janky-last-build (repo branch)
+  (interactive (janky-read-repo-branch-args))
+  (elt (janky-status repo branch) 0))
+
+(defun janky-show-status (repo branch)
+  (interactive (janky-read-repo-branch-args))
+  (let ((status (plist-get (janky-last-build repo branch) :green)))
+    (if (equal status t)
+        "Build success!"
+      "Build failed!")))
 
 (provide 'janky)
